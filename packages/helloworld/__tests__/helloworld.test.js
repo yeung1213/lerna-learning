71b544586a28207665c73feb540676b8d2a2a65a
yeung1213 <yeung2423@gmail.com>
2023-08-29 15:56:45 +0800

    update hello world

--- packages/helloworld/__tests__/helloworld.test.js
+++ packages/helloworld/__tests__/helloworld.test.js
@@ -1,7 +1,8 @@
-'use strict';
+"use strict";
 
-const helloworld = require('..');
-const assert = require('assert').strict;
+const { helloworld, helloworldv2 } = require("..");
+const assert = require("assert").strict;
 
-assert.strictEqual(helloworld(), 'Hello from helloworld');
-console.info('helloworld tests passed');
+assert.strictEqual(helloworld(), "Hello from helloworld");
+assert.strictEqual(helloworldv2(), "Hello from helloworld v2");
+console.info("helloworld tests passed");
--- packages/helloworld/lib/helloworld.js
+++ packages/helloworld/lib/helloworld.js
@@ -1,7 +1,14 @@
-'use strict';
+"use strict";
 
-module.exports = helloworld;
+module.exports = {
+  helloworld,
+  helloworldv2,
+};
 
 function helloworld() {
-  return 'Hello from helloworld';
+  return "Hello from helloworld";
+}
+
+function helloworldv2() {
+  return "Hello from helloworld v2";
 }
