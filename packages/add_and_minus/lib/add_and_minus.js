054471123babb9b0cf5d6248d3e3e4a95430ab50
yeung1213 <yeung2423@gmail.com>
2023-08-30 11:32:13 +0800

    update

--- packages/add_and_minus/lib/add_and_minus.js
+++ packages/add_and_minus/lib/add_and_minus.js
@@ -4,6 +4,6 @@ module.exports = addAndMinus;
 const addv2 = require("@simonooo/add");
 
 function addAndMinus(x, y, z) {
-  console.log('hello')
+  console.log('hello2')
   return addv2(x, y) - z;
 }
